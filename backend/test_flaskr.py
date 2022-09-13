ed44fa5792a59a7cd902b211ef1bd4614a7a5978
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-09-13 04:08:50 +0100

    test: wrote test cases for create questions endpoint for expected cases and handled associated error

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -19,6 +19,13 @@ class TriviaTestCase(unittest.TestCase):
             "student", "student", 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
+        self.new_question = {
+            'question': 'What is the powerhouse of a cell?',
+            'answer': 'Mitochondria',
+            'difficulty': 3,
+            'category': 1
+        }
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -81,7 +88,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertEqual(data['deleted'], 1)
         self.assertEqual(question, None)
-    
+
     def test_404_if_question_does_not_exist(self):
         res = self.client().delete('/questions/1000')
         data = json.loads(res.data)
@@ -89,6 +96,22 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'resource not found')
+
+    def test_create_new_question(self):
+        res = self.client().post('/questions', json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+
+    def test_405_if_question_creation_not_allowed(self):
+        res = self.client().post('/questions/45', json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')
 
 
 # Make the tests conveniently executable
